3b37ca37a93b7143b4ad79d3fd7ac2391609b99a
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-16 14:26:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
-<!--_Require once--->
-    <h1>Welcome to the store </h1>
 <?php
 include('header.php');
 ?>
+<!--_Require once--->
+    <h1>Welcome to the store </h1>
+
     <form action="handlepost.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+Product Name: <input type="text" name="name"><br>
+Quantity: <input type="text" name="name"><br>
+Your contact information: <input type="text" name="email"><br>
 <input type="submit">
 </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
