92e5ca78885c310e4199aa5a65580e3537e0120d
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-18 22:56:58 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -6,6 +6,10 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   </head>
+  <?php
+include('header.php');
+
+?>
   <body>
     <h1>Post Response</h1>
       Welcome <?php echo $_POST["name"]; ?><br>
