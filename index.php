bc5184c4efc40425aedeb081070b34a02ead5688
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-18 22:54:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,11 +16,14 @@ Your contact information: <input type="text" name="email"><br>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
 
   </body>
+<Body>
+    
 <footer>
     <?php
 include('from.php');
 ?>
 </footer>
+    </body>
 
 
 </html>
