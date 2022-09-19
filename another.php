799e12904696590dd7166d7270a20c9ab71ca254
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-18 23:03:53 -0500

    Update and rename from.php to another.php

---
+++ another.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <?php
+include('header.php');
+
+?>
+  <body>
+     <?php
+   if( $_GET["name"] || $_GET["age"] ) {
+      echo "Welcome ". $_GET['name']. "<br />";
+      echo "You are ". $_GET['age']. " years old.";
+      
+      exit();
+   }
+?>
+<html>
+   <body>
+   
+      <form action = "<?php $_PHP_SELF ?>" method = "GET">
+         Name: <input type = "text" name = "name" />
+         Age: <input type = "text" name = "age" />
+         <input type = "submit" />
+      </form>
+      
+   </body>
+</html>
--- from.php
+++
@@ -1,4 +0,0 @@
-<footer>
-  <p>Author: Olivia Owen</p>
-  <p><a href="mailto:olivia.a.owen-1@ou.edu">Olivia.A.Owen-1@ou.edu</a></p>
-</footer>
