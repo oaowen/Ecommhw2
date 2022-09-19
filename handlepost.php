b1b8c1a9c28b5bc1cbb3b9f66cec3eed55351ea4
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-18 23:08:06 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -11,12 +11,12 @@ include('header.php');
 
 ?>
   <body>
-    <h1>Post Response</h1>
+    <h1>Your Response</h1>
       Welcome you want <?php echo $_POST["prodname"]; ?><br>
      at a quantity of <?php echo $_POST["quanname"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
    
-    <a class="btn btn-primary" href="another.php" role="button">Add Another</a>
+    <a class="btn btn-primary" href="another.php" role="button">Any special requests?</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
