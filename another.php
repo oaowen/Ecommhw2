12badd3811f6899e0907424e6d9bd3aaa238c95e
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-18 23:09:19 -0500

    Update another.php

--- another.php
+++ another.php
@@ -14,7 +14,7 @@ include('header.php');
      <?php
    if( $_GET["name"] || $_GET["age"] ) {
       echo "Welcome ". $_GET['name']. "<br />";
-      echo "You are ". $_GET['age']. " years old.";
+      echo "Your request is ". $_GET['age']. " Thank you for your request.";
       
       exit();
    }
@@ -24,7 +24,7 @@ include('header.php');
    
       <form action = "<?php $_PHP_SELF ?>" method = "GET">
          Name: <input type = "text" name = "name" />
-         Age: <input type = "text" name = "age" />
+         Request: <input type = "text" name = "request" />
          <input type = "submit" />
       </form>
       
