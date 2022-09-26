3ed5f9a7f3c20bd88b75a64de8a6edbd6b71d6d6
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-26 14:14:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ Your contact information: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 <?php
-function writeMsg() {
-  echo "Please remember to submit!";
+function PrivacyMessage() {
+  <button type="button" class="btn btn-lg btn-danger" data-bs-toggle="popover" data-bs-title="Privacy Notice" data-bs-content="Your conatct infromation will not be leaked. We uphold the highest level of privacy for our clients, rest assured">Click to toggle popover</button>
 }
 
-writeMsg(); // call the function
+PrivacyMessage(); 
 ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
