fb1411a068034551e9d1f1cdd2c9990eba8a7d2b
oaowen <89744805+oaowen@users.noreply.github.com>
2022-09-28 13:34:34 -0500

    Update handlepost.php

--- handlepost.php
+++ handlepost.php
@@ -15,6 +15,10 @@ include('header.php');
       Welcome you want <?php echo $_POST["prodname"]; ?><br>
      at a quantity of <?php echo $_POST["quanname"]; ?><br>
 Your contact info is: <?php echo $_POST["email"]; ?>
+    <?php
+include('footer.php');
+
+?>
    
     <a class="btn btn-primary" href="another.php" role="button">Any special requests?</a>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
